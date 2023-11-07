7e45048aae8cfebfb41f9cd47217bb0ac76570e3
Michael Lin <linmichael02@gmail.com>
2023-11-06 21:05:40 -0500

    Added tests for word_deterministic

---
+++ tests/test_word_deterministic.py
@@ -0,0 +1,28 @@
+from brawndo.brawndo import word_deterministic
+
+from io import StringIO
+import sys
+from contextlib import redirect_stderr
+import pytest
+
+from typing import Optional
+
+texts = [
+    "The be to of and",
+    "a in that have I",
+    "it for not on with.",
+]
+shifts = [0, 1, 2]
+desireds = [
+    "The,14\nbe,5\nto,1\nof,3\nand,0\n",
+    "a,1\nin,7\nthat,1\nhave,4\nI,9\n",
+    "it,15\nfor,10\nnot,4\non,15\nwith.,15\n"
+]
+
+# Zip all together
+params = zip(texts, shifts, desireds)
+
+@pytest.mark.parametrize("text,shift,desired", params)
+def test_rainbow_deterministic(text, shift, desired):
+    test_vector = word_deterministic(text, shift, testing=True)
+    assert test_vector == desired
